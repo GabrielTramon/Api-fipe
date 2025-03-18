a574a71143c536383af65bd900a245dda932868a
GabrielTramon <gabrieljoaquim080706@gmail.com>
2025-03-18 10:38:38 -0300

    dados de valor finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 async function apiCar() {
-    const urlMarca = `https://parallelum.com.br/fipe/api/v1/carros/marcas`
+    const urlPadrao = `https://parallelum.com.br/fipe/api/v1/carros/marcas`
     const marcaSelect = document.getElementById("marca") //ele pega o id do select
     const modeloSelect = document.getElementById("modelo") //ele pega o id do modelo
     const anoSelect = document.getElementById("ano") //ele pega o id do modelo
+    const valorCarro = document.getElementById("valor")
 
     let codMarca;
     let codModelo;
+    let codAno;
 
     try {
-        const response = await fetch(urlMarca); //cria requisição com a url
+        const response = await fetch(urlPadrao); //cria requisição com a url
         const marcas = await response.json(); //transforma a response em json
 
         marcaSelect.innerHTML = "<option value=''>Selecione uma marca</option>"; // insere o item no html
@@ -21,12 +23,15 @@ async function apiCar() {
         });
 
         marcaSelect.addEventListener("change", async (e) => { //addEventListener ele captura uma ação que nesse caso é alterar
+            modeloSelect.innerHTML = "<option value=''>Selecione um modelo</option>";
+            anoSelect.innerHTML = "<option value=''>Selecione um ano</option>";
+            valorCarro.innerHTML = ""; // Limpa o valor do carro
+            
             codMarca = e.target.value //pegando o valor do evendo "e"
-            const urlModelo = `https://parallelum.com.br/fipe/api/v1/carros/marcas/${codMarca}/modelos`
-            const response = await fetch(urlModelo); //cria requisição com a url
+            const response = await fetch(`${urlPadrao}/${codMarca}/modelos`); //cria requisição com a url
             const modelos = await response.json(); //transforma a response em json
-
-            modeloSelect.innerHTML = "<option value=''>Selecione uma marca</option>"; // insere o item no html
+            
+            modeloSelect.innerHTML = "<option value=''>Selecione uma modelo</option>"; // insere o item no html
 
             modelos.modelos.forEach(modelo => {
                 const optionModelo = document.createElement("option"); //criar um elemento opções
@@ -38,11 +43,10 @@ async function apiCar() {
         
         modeloSelect.addEventListener("change", async (e) => { //addEventListener ele captura uma ação que nesse caso é alterar
             codModelo = e.target.value //pegando o valor do evendo "e"
-            const urlAno = `https://parallelum.com.br/fipe/api/v1/carros/marcas/${codMarca}/modelos/${codModelo}/anos`
-            const response = await fetch(urlAno); //cria requisição com a url
+            const response = await fetch(`${urlPadrao}/${codMarca}/modelos/${codModelo}/anos`); //cria requisição com a url
             const anos = await response.json(); //transforma a response em json
 
-            anoSelect.innerHTML = "<option value=''>Selecione uma marca</option>"; // insere o item no html
+            anoSelect.innerHTML = "<option value=''>Selecione uma ano</option>"; // insere o item no html
 
             anos.forEach(ano => {
                 const optionAno = document.createElement("option"); //criar um elemento opções
@@ -50,6 +54,15 @@ async function apiCar() {
                 optionAno.textContent = ano.nome; //nome inserido
                 anoSelect.appendChild(optionAno);//atribuir  o select com as opções
             });
+        });
+
+        anoSelect.addEventListener("change", async (e) => { //addEventListener ele captura uma ação que nesse caso é alterar
+            codAno = e.target.value //pegando o valor do evendo "e"
+            const response = await fetch(`${urlPadrao}/${codMarca}/modelos/${codModelo}/anos/${codAno}`); //cria requisição com a url
+            const Valor = await response.json(); //transforma a response em json
+
+            const resultadoValor = Valor.Valor
+            valorCarro.innerHTML = resultadoValor; // insere o item no html
         });
 
     } catch (error) {
